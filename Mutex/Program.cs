6cbf4b541230209d25788b94163e6c70dd214858
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-10-18 02:33:36 +0300

    Initial commit

---
+++ Mutex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mutex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
