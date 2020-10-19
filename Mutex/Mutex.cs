befd2da89cf1f5caeecbf290535c92398925ecf9
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-10-19 16:21:57 +0300

    Update Mutex.cs

--- Mutex/Mutex.cs
+++ Mutex/Mutex.cs
@@ -1,20 +1,27 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
 namespace Mutex
 {
     public class Mutex
     {
-        private int _lockCounter;
+        private const int UnlockedId = -1;
+
+        private int _lockThreadId = UnlockedId;
+
+        private static int CurrentThreadId => Thread.CurrentThread.ManagedThreadId;
 
         public void Lock()
         {
             var sw = new SpinWait();
-            while (Interlocked.CompareExchange(ref _lockCounter, 1, 0) == _lockCounter) sw.SpinOnce();
+            while (Interlocked.CompareExchange(ref _lockThreadId, CurrentThreadId, UnlockedId) != UnlockedId)
+                sw.SpinOnce();
         }
 
         public void Unlock()
         {
-            Interlocked.Exchange(ref _lockCounter, 0);
+            if (Interlocked.CompareExchange(ref _lockThreadId, UnlockedId, CurrentThreadId) != CurrentThreadId)
+                throw new Exception("Unable to unlock Mutex by " + CurrentThreadId + " Thread");
         }
     }
 }
